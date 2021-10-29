3d3a65b20fec56d164c5c81d2d588f8033c8d121
Elvis C Espinoza Navarrete <elvisespinoza@Elviss-MacBook-Air.local>
2021-10-28 20:20:58 -0400

    added constant, variable, and chached elements

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // constant
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/'
 
-// statevariables
+// state variables
+let pokemonData;
 
-//cached element references
+// cached element references
+const $main = $('main');
+const $form = $('form');
+const $input = $("input[type='text']");
+const $name = $('#name');
+const $moves = $('#moves');
+const $abilities = $('#abilities');
+const $height = $('#height');
 
 // event listeners
 
