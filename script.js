c09a219c935d7fe9b427e01e11fb9ba0aceb1704
Elvis C Espinoza Navarrete <elvisespinoza@Elviss-MacBook-Air.local>
2021-11-02 18:23:44 -0400

    wip: last touches

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+$(function(){
+
 // constant
 const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/'
 
@@ -12,34 +14,44 @@ const $name = $('#name');
 const $moves = $('#moves');
 const $stats = $('#stats');
 const $id = $('#id');
+const $randomizer = $("input[value='Randomizer']")
 
 // event listeners
 $form.on('submit', handleSearch)
+$randomizer.on('click', handleRandomizer)
+
+// $randomizer.on("click", function(evt) {
+//     evt.preventDefault()
 
 // functions
 function handleSearch(e){
     e.preventDefault();
     const pokemonName = $input.val();
-
     $.ajax(`${BASE_URL}${pokemonName}`)
         .then(function(data){
             pokemonData = data
             render();
         }, function(error){
-            console.log(error);
+            console.log("error here: ", error);
         });
 }
 
 function render(){
     $main.html(`
-    <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}" />
-    <p>Pokedex Number: </p>
-    <p id="id"># ${pokemonData.id}</p>
-    <p>Name: </p>
-    <p id="name">${pokemonData.name}</p>
-    <p>Move: </p>
-    <p id="moves">${pokemonData.moves[0].move.name}, ${pokemonData.moves[1].move.name} & ${pokemonData.moves[5].move.name}</p>
-    <p>Base Stat: </p>
-    <p id="stats">${pokemonData.stats[0].base_stat}</p>
+    <img id='img' src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}" />
+    <p class='mainEl' id='idEl' >Pokedex Number: </p>
+    <p class='mainEl' id="id"># ${pokemonData.id}</p>
+    <p class='mainEl' id='nameEl' >Name: </p>
+    <p class='mainEl' id="name">${pokemonData.name}</p>
+    <p class='mainEl' id='moveEl' >Move: </p>
+    <p class='mainEl' id="move">${pokemonData.moves[16].move.name}</p>
+    <p class='mainEl' id='statEl' >Base Stat: </p>
+    <p class='mainEl' id="stat">${pokemonData.stats[0].base_stat}</p>
     `);
 }
+
+
+
+
+
+})
